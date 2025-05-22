99a0b1013ed6a829218192087d090d7fc9c05625
luckycyang <kfove.liang@protonmail.com>
2025-05-22 21:18:30 +0800

    like ftdi

--- probe-rs/src/probe/ch347usbjtag/mod.rs
+++ probe-rs/src/probe/ch347usbjtag/mod.rs
@@ -5,6 +5,8 @@ use protocol::Ch347UsbJtagDevice;
 
 use crate::probe::{DebugProbe, ProbeFactory};
 
+use super::{AutoImplementJtagAccess, JtagDriverState, RawJtagIo};
+
 #[derive(Debug)]
 pub struct Ch347UsbJtagFactory;
 
@@ -17,6 +19,7 @@ impl std::fmt::Display for Ch347UsbJtagFactory {
 #[derive(Debug)]
 pub struct Ch347UsbJtag {
     device: Ch347UsbJtagDevice,
+    jtag_state: JtagDriverState,
 }
 
 impl ProbeFactory for Ch347UsbJtagFactory {
@@ -26,13 +29,43 @@ impl ProbeFactory for Ch347UsbJtagFactory {
     ) -> Result<Box<dyn super::DebugProbe>, super::DebugProbeError> {
         let ch347 = Ch347UsbJtagDevice::new_from_selector(selector)?;
 
-        Ok(Box::new(Ch347UsbJtag { device: ch347 }))
+        Ok(Box::new(Ch347UsbJtag {
+            device: ch347,
+            jtag_state: JtagDriverState::default(),
+        }))
     }
 
     fn list_probes(&self) -> Vec<super::DebugProbeInfo> {
         protocol::list_ch347usbjtag_devices()
     }
 }
+
+impl RawJtagIo for Ch347UsbJtag {
+    fn shift_bit(
+        &mut self,
+        tms: bool,
+        tdi: bool,
+        capture: bool,
+    ) -> Result<(), super::DebugProbeError> {
+        self.jtag_state.state.update(tms);
+        self.device.shift_bit(tms, tdi, capture)?;
+        Ok(())
+    }
+
+    fn read_captured_bits(&mut self) -> Result<bitvec::prelude::BitVec, super::DebugProbeError> {
+        self.device.read_captured_bits()
+    }
+
+    fn state_mut(&mut self) -> &mut JtagDriverState {
+        &mut self.jtag_state
+    }
+
+    fn state(&self) -> &JtagDriverState {
+        &self.jtag_state
+    }
+}
+
+impl AutoImplementJtagAccess for Ch347UsbJtag {}
 
 impl DebugProbe for Ch347UsbJtag {
     fn get_name(&self) -> &str {
@@ -88,5 +121,9 @@ impl DebugProbe for Ch347UsbJtag {
 
     fn into_probe(self: Box<Self>) -> Box<dyn DebugProbe> {
         self
+    }
+
+    fn try_as_jtag_probe(&mut self) -> Option<&mut dyn super::JtagAccess> {
+        Some(self)
     }
 }
--- probe-rs/src/probe/ch347usbjtag/protocol.rs
+++ probe-rs/src/probe/ch347usbjtag/protocol.rs
@@ -1,10 +1,17 @@
+use std::time::Duration;
+
+use bitvec::vec::BitVec;
 use nusb::{DeviceInfo, Interface};
 
-use crate::probe::{self, DebugProbeInfo, DebugProbeSelector, ProbeCreationError};
+use crate::probe::{
+    self, DebugProbeError, DebugProbeInfo, DebugProbeSelector, ProbeCreationError, RawJtagIo,
+    usb_util::InterfaceExt,
+};
 
 use super::Ch347UsbJtagFactory;
 
 const CH34X_VID_PID: [(u16, u16); 3] = [(0x1A86, 0x55DE), (0x1A86, 0x55DD), (0x1A86, 0x55E8)];
+const REV_BUF_MAX: usize = 64;
 
 pub(crate) fn is_ch34x_device(device: &DeviceInfo) -> bool {
     CH34X_VID_PID.contains(&(device.vendor_id(), device.product_id()))
@@ -16,12 +23,49 @@ enum PACK {
     LARGER_PACK,
 }
 
+enum Command {
+    JTAG_INIT,
+    JTAG_BIT_OP,
+    JTAG_BIT_OP_RD,
+    JTAG_DATA_SHIFT,
+    JTAG_DATA_SHIFT_RD,
+}
+
+struct Clock {
+    tms: bool,
+    tdi: bool,
+    trst: bool,
+}
+
+impl From<Clock> for u8 {
+    fn from(value: Clock) -> Self {
+        let Clock { tms, tdi, trst } = value;
+        0 | u8::from(tms) << 1 | u8::from(tdi) << 4 | u8::from(trst) << 5
+    }
+}
+
+impl From<Command> for u8 {
+    fn from(value: Command) -> Self {
+        match value {
+            Command::JTAG_INIT => 0xD0,
+            Command::JTAG_BIT_OP => 0xD1,
+            Command::JTAG_BIT_OP_RD => 0xD2,
+            Command::JTAG_DATA_SHIFT => 0xD3,
+            Command::JTAG_DATA_SHIFT_RD => 0xD4,
+        }
+    }
+}
+
 pub struct Ch347UsbJtagDevice {
     device: Interface,
     name: String,
+    comand_quene: Option<Command>,
+    output_buffer: Vec<u8>,
+    response: BitVec,
     epout: u8,
     epin: u8,
     pack: Option<PACK>,
+    speed_khz: u32,
 }
 
 impl std::fmt::Debug for Ch347UsbJtagDevice {
@@ -31,6 +75,7 @@ impl std::fmt::Debug for Ch347UsbJtagDevice {
             .field("epout", &self.epout)
             .field("epin", &self.epin)
             .field("pack", &self.pack)
+            .field("speed", &self.speed_khz)
             .finish()
     }
 }
@@ -77,10 +122,112 @@ impl Ch347UsbJtagDevice {
         Ok(Self {
             device: interface,
             name: "ch347".into(),
+            comand_quene: None,
+            output_buffer: Vec::new(),
+            response: BitVec::new(),
             epout: 0x06,
             epin: 0x86,
             pack: None,
+            speed_khz: 15000,
         })
+    }
+
+    pub fn attach(&mut self) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    fn speed_khz(&self) -> u32 {
+        self.speed_khz
+    }
+
+    fn set_speed_khz(&mut self, speed_khz: u32) -> u32 {
+        self.speed_khz = speed_khz;
+        self.speed_khz
+    }
+
+    fn pack(&self) -> &Option<PACK> {
+        &self.pack
+    }
+
+    fn apply_clock_speed(&mut self, speed_khz: u32) -> Result<u32, DebugProbeError> {
+        let index;
+        let mut buf = [0; 4];
+        match *self.pack() {
+            Some(PACK::STANDARD_PACK) => {
+                index = match speed_khz {
+                    1875 => 0,
+                    3750 => 1,
+                    7500 => 2,
+                    15000 => 3,
+                    30000 => 4,
+                    60000 => 5,
+                    _ => return Err(DebugProbeError::UnsupportedSpeed(speed_khz)),
+                };
+            }
+            Some(PACK::LARGER_PACK) => {
+                index = match speed_khz {
+                    468 => 0,
+                    937 => 1,
+                    1875 => 2,
+                    3750 => 3,
+                    7500 => 4,
+                    15000 => 5,
+                    30000 => 6,
+                    60000 => 7,
+                    _ => return Err(DebugProbeError::UnsupportedSpeed(speed_khz)),
+                }
+            }
+            None => return Err(DebugProbeError::UnsupportedSpeed(speed_khz)),
+        }
+        self.device
+            .write_bulk(
+                self.epout,
+                &[0xD0, 0x06, 0x00, 0x00, index, 0x00, 0x00, 0x00, 0x00],
+                Duration::from_millis(500),
+            )
+            .map_err(DebugProbeError::Usb)?;
+
+        self.device
+            .read_bulk(self.epin, &mut buf, Duration::from_millis(500))
+            .map_err(DebugProbeError::Usb)?;
+        if buf[3] == 0x00 {
+            Ok(speed_khz)
+        } else {
+            Err(DebugProbeError::UnsupportedSpeed(speed_khz))
+        }
+    }
+
+    fn read_response(&mut self) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    fn flush(&mut self) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    fn append_command(&mut self, command: Command) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    fn finalize_command(&mut self) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    pub(crate) fn shift_bit(
+        &mut self,
+        tms: bool,
+        tdi: bool,
+        capture: bool,
+    ) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    pub(crate) fn send_buffer(&mut self) -> Result<(), DebugProbeError> {
+        Ok(())
+    }
+
+    pub(crate) fn read_captured_bits(&mut self) -> Result<BitVec, DebugProbeError> {
+        todo!()
     }
 }
 
